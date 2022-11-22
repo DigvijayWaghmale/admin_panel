291d381367c216e32fec7da41449d3659a0dcff9
DigvijayWaghmale <dwaghmale18@gmail.com>
2022-11-22 13:10:06 +0530

    little bit changes in navbar and sidebar.

--- rn53themes.net/header.php
+++ rn53themes.net/header.php
@@ -1,61 +1,88 @@
 <div class="container-fluid sb1">
-        <div class="row">
-            <!--== LOGO ==-->
-            <div class="col-md-2 col-sm-3 col-xs-6 sb1-1">
-                <a href="#" class="btn-close-menu"><i class="fa fa-times" aria-hidden="true"></i></a>
-                <a href="#" class="atab-menu"><i class="fa fa-bars tab-menu" aria-hidden="true"></i></a>
-                <a href="index.php" class="logo"><img src="images/furnishing.png" alt="" >
-                </a>
-            </div>
-            <!--== SEARCH ==-->
-            <div class="col-md-6 col-sm-6 mob-hide">
-                <!-- <form class="app-search">
+    <div class="row">
+        <!--== LOGO ==-->
+        <div class="col-md-2 col-sm-3 col-xs-6 sb1-1">
+            <a href="#" class="btn-close-menu"><i class="fa fa-times" aria-hidden="true"></i></a>
+            <a href="#" class="atab-menu"><i class="fa fa-bars tab-menu" aria-hidden="true"></i></a>
+            <a href="index.php" class="logo"><img src="images/furnishing.png" alt="">
+            </a>
+        </div>
+        <!--== SEARCH ==-->
+        <div class="col-md-6 col-sm-6 mob-hide">
+            <!-- <form class="app-search">
                     <input type="text" placeholder="Search..." class="form-control">
                     <a href="#"><i class="fa fa-search"></i></a>
                 </form> -->
-                <ul class="navlist1">
-                   <li><a href="index.php">DASHBOARD</a></li> 
-                   <li><a href="javascript:void(0)" class="collapsible-header">LISTING</a></li> 
-                   <li><a href="javascript:void(0)">USERS</a></li> 
-                   <li><a href="javascript:void(0)">BOOKING AND ENQUIRY</a></li> 
+            <ul class="navlist1">
+                <li><a href="index.php" class="collapsible-header">DASHBOARD</a></li>
+                <a class='waves-effect dropdown-button ' href='#' data-activates='top-menu1'>LISTING</i>
+                </a>
+                <ul id='top-menu1' class='dropdown-content top-menu-sty'>
+                    <li><a href="listing-all.php" class="waves-effect"></i>ALL LISTING</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li><a href="listing-add.php" class="ho-dr-con-last waves-effect"></i>ADD NEW LISTING</a>
+                    </li>
+                    <li><a href="listing-cat-all.php" class="ho-dr-con-last waves-effect"></i>ALL LISTING CATEGORIES</a>
+                    </li>
                 </ul>
-            </div>
-            <!--== NOTIFICATION ==-->
-            <div class="col-md-2 tab-hide">
-                <!-- <div class="top-not-cen">
+
+                <a class='waves-effect dropdown-button ' href='#' data-activates='top-menu2'>USERS</i>
+                </a>
+                <ul id='top-menu2' class='dropdown-content top-menu-sty'>
+                    <li><a href="user-add.php" class="waves-effect"></i>ADD NEW USER</a>
+                    </li>
+
+                </ul>
+
+                
+                <a class='waves-effect dropdown-button ' href='#' data-activates='top-menu3'>BOOKING AND ENQUIRY</i>
+                </a>
+                <ul id='top-menu3' class='dropdown-content top-menu-sty'>
+                    <li><a href="hotel-booking-all.php" class="waves-effect"></i>HOTEL BOOKING LIST</a>
+                    </li>
+
+                </ul>
+
+                
+
+        </div>
+        <!--== NOTIFICATION ==-->
+        <div class="col-md-2 tab-hide">
+            <!-- <div class="top-not-cen">
                     <a class='waves-effect btn-noti' href='#'><i class="fa fa-commenting-o" aria-hidden="true"></i><span>5</span></a>
                     <a class='waves-effect btn-noti' href='#'><i class="fa fa-envelope-o" aria-hidden="true"></i><span>5</span></a>
                     <a class='waves-effect btn-noti' href='#'><i class="fa fa-tag" aria-hidden="true"></i><span>5</span></a>
                 </div> -->
-            </div>
-            <!--== MY ACCCOUNT ==-->
-            <div class="col-md-2 col-sm-3 col-xs-6">
-                <!-- Dropdown Trigger -->
-                <a class='waves-effect dropdown-button top-user-pro' href='#' data-activates='top-menu'><img src="images/user/6.png" alt="" />My Account <i class="fa fa-angle-down" aria-hidden="true"></i>
-                </a>
+        </div>
+        <!--== MY ACCCOUNT ==-->
+        <div class="col-md-2 col-sm-3 col-xs-6">
+            <!-- Dropdown Trigger -->
+            <a class='waves-effect dropdown-button top-user-pro' href='#' data-activates='top-menu'><img src="images/user/6.png" alt="" />My Account <i class="fa fa-angle-down" aria-hidden="true"></i>
+            </a>
 
-                <!-- Dropdown Structure -->
-                <ul id='top-menu' class='dropdown-content top-menu-sty'>
-                    <!-- <li><a href="setting.php" class="waves-effect"><i class="fa fa-cogs" aria-hidden="true"></i>Admin Setting</a>
+            <!-- Dropdown Structure -->
+            <ul id='top-menu' class='dropdown-content top-menu-sty'>
+                <!-- <li><a href="setting.php" class="waves-effect"><i class="fa fa-cogs" aria-hidden="true"></i>Admin Setting</a>
                     </li> -->
-                    <!-- <li><a href="listing-all.php" class="waves-effect"><i class="fa fa-list-ul" aria-hidden="true"></i> Listings</a>
+                <!-- <li><a href="listing-all.php" class="waves-effect"><i class="fa fa-list-ul" aria-hidden="true"></i> Listings</a>
                     </li> -->
-                    <!-- <li><a href="hotel-all.php" class="waves-effect"><i class="fa fa-building-o" aria-hidden="true"></i> Hotels</a>
+                <!-- <li><a href="hotel-all.php" class="waves-effect"><i class="fa fa-building-o" aria-hidden="true"></i> Hotels</a>
                     </li> -->
-                    <!-- <li><a href="package-all.php" class="waves-effect"><i class="fa fa-umbrella" aria-hidden="true"></i> Tour Packages</a>
+                <!-- <li><a href="package-all.php" class="waves-effect"><i class="fa fa-umbrella" aria-hidden="true"></i> Tour Packages</a>
                     </li> -->
-                    <!-- <li><a href="event-all.php" class="waves-effect"><i class="fa fa-flag-checkered" aria-hidden="true"></i> Events</a>
+                <!-- <li><a href="event-all.php" class="waves-effect"><i class="fa fa-flag-checkered" aria-hidden="true"></i> Events</a>
                     </li> -->
-                    <!-- <li><a href="offers.php" class="waves-effect"><i class="fa fa-tags" aria-hidden="true"></i> Offers</a>
+                <!-- <li><a href="offers.php" class="waves-effect"><i class="fa fa-tags" aria-hidden="true"></i> Offers</a>
                     </li> -->
-                    <li><a href="user-add.php" class="waves-effect"><i class="fa fa-user-plus" aria-hidden="true"></i> Add New User</a>
-                    </li>
-                    <!-- <li><a href="#" class="waves-effect"><i class="fa fa-undo" aria-hidden="true"></i> Backup Data</a>
+                <li><a href="user-add.php" class="waves-effect"><i class="fa fa-user-plus" aria-hidden="true"></i> Add New User</a>
+                </li>
+                <!-- <li><a href="#" class="waves-effect"><i class="fa fa-undo" aria-hidden="true"></i> Backup Data</a>
                     </li> -->
-                    <li class="divider"></li>
-                    <li><a href="#" class="ho-dr-con-last waves-effect"><i class="fa fa-sign-in" aria-hidden="true"></i> Logout</a>
-                    </li>
-                </ul>
-            </div>
+                <li class="divider"></li>
+                <li><a href="#" class="ho-dr-con-last waves-effect"><i class="fa fa-sign-in" aria-hidden="true"></i> Logout</a>
+                </li>
+            </ul>
         </div>
     </div>
+</div>
